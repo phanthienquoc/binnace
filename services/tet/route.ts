ccaab01a14e9df5a4409d47e82601f74bdd26046
Quoc Phan <quocpt@inspirelab.io>
2023-01-18 17:34:52 +0700

    close year

--- services/tet/route.ts
+++ services/tet/route.ts
@@ -5,22 +5,12 @@ const apiRoute = (app: Express) => {
   app.route('/tet').get((req: Request, res: Response) => {
     res.send(`Còn ${getDistance()} là tết nha!!!`);
   });
-
-  app.route('/ducfake').get((req: Request, res: Response) => {
-    res.send(`Còn ${getDistance('01/14/2023')} là ILT no more nha!!!`);
-  });
 };
 
 export const tetTelegramRoute = (telegram: any = null) => {
   if (telegram) {
     telegram.onCommand(/\/tet/, (msg: any) => {
       telegram.sendMessage(msg.chat.id, `Còn ${getDistance()} là tết nha!!!`);
-    });
-    telegram.onCommand(/\/ducfake/, (msg: any) => {
-      telegram.sendMessage(
-        msg.chat.id,
-        `Còn ${getDistance('01/14/2023')} là ILT no more nha!!!`
-      );
     });
   }
 };
